a2e92af2d374fc2863516416c8c33a3d2b5fd098
DaniBacheva <y.bacheva@abv.bg>
2024-03-16 16:41:17 +0200

    Add new theme service

--- workshop/demo/src/app/api.service.ts
+++ workshop/demo/src/app/api.service.ts
@@ -23,6 +23,13 @@ export class ApiService {
     return this.http.get<Theme>(`${apiUrl}/themes/${id}`)
   }
 
+  createTheme (
+    themeName:string,
+    postText:string 
+    ) {
+      return this.http.post<Theme>('/api/themes' , {themeName, postText})
+    }
+
   getPosts(limit?: number) {
     const { apiUrl } = environment;
 
--- workshop/demo/src/app/themes-list/new-theme/new-theme.component.ts
+++ workshop/demo/src/app/themes-list/new-theme/new-theme.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { Router } from '@angular/router';
+import { ApiService } from 'src/app/api.service';
 
 @Component({
   selector: 'app-new-theme',
@@ -9,12 +11,18 @@ import { NgForm } from '@angular/forms';
 
 export class NewThemeComponent {
 
+  constructor(private apiService: ApiService, private router: Router) { }
 
+  newThemeHandler(form: NgForm): void {
+    if (form.invalid) {
+      return
+    }
+    console.log(form.value)
+    const { themeName, postText } = form.value;
+    this.apiService.createTheme(themeName, postText).subscribe(() => {
+      this.router.navigate(['/themes'])
+    }
+    )
 
-newThemeHandler ( form: NgForm) : void {
- if (form.invalid) {
-  return
- }
- console.log(form.value)
-}
+  }
 }
